9d2df1ce7c8c3934c696e22d0c455e4ef6f0bff4
Kqwerty25 <did.dikopolov@mail.ru>
2025-04-14 11:09:44 +0300

    version 1.0

---
+++ WebApplication2/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Управление общими сведениями о сборке осуществляется с помощью 
+' набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+' связанных с этой сборкой.
+
+' Проверка значений атрибутов сборки
+<Assembly: AssemblyTitle("WebApplication2")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WebApplication2")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов typelib, если этот проект видим для COM
+<Assembly: Guid("3fa20679-9917-4ccd-a413-31f7263409ac")>
+
+' Сведения о версии сборки состоят из указанных ниже четырех значений:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Можно задать все значения или принять номер сборки и номер редакции по умолчанию, 
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
